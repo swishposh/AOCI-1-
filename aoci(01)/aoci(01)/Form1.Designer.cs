bb224433d038773a5c9fa8c7b77596cffcbea12b
kaanaizer <kannaizer@users.noreply.github.com>
2020-10-18 12:26:34 +0700

    001

---
+++ aoci(01)/aoci(01)/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace aoci_01_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.process = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.vScrollBar2 = new System.Windows.Forms.VScrollBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.vScrollBar3 = new System.Windows.Forms.VScrollBar();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(0, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(640, 480);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(653, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(739, 12);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(640, 480);
+            this.imageBox2.TabIndex = 4;
+            this.imageBox2.TabStop = false;
+            // 
+            // process
+            // 
+            this.process.Location = new System.Drawing.Point(653, 335);
+            this.process.Name = "process";
+            this.process.Size = new System.Drawing.Size(75, 23);
+            this.process.TabIndex = 5;
+            this.process.Text = "process";
+            this.process.UseVisualStyleBackColor = true;
+            this.process.Click += new System.EventHandler(this.process_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(653, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "WebCam";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(653, 264);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "video";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 33;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(653, 12);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 186);
+            this.vScrollBar1.TabIndex = 10;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(653, 374);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // vScrollBar2
+            // 
+            this.vScrollBar2.Location = new System.Drawing.Point(683, 12);
+            this.vScrollBar2.Name = "vScrollBar2";
+            this.vScrollBar2.Size = new System.Drawing.Size(17, 186);
+            this.vScrollBar2.TabIndex = 10;
+            this.vScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar2_Scroll);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(653, 401);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // vScrollBar3
+            // 
+            this.vScrollBar3.LargeChange = 50;
+            this.vScrollBar3.Location = new System.Drawing.Point(711, 12);
+            this.vScrollBar3.Maximum = 200;
+            this.vScrollBar3.Minimum = 100;
+            this.vScrollBar3.Name = "vScrollBar3";
+            this.vScrollBar3.Size = new System.Drawing.Size(17, 186);
+            this.vScrollBar3.TabIndex = 13;
+            this.vScrollBar3.Value = 100;
+            this.vScrollBar3.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar3_Scroll);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(653, 428);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(75, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 514);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.vScrollBar3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.vScrollBar2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.process);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button process;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.VScrollBar vScrollBar2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.VScrollBar vScrollBar3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ aoci(01)/aoci(01)/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace aoci_01_
+{
+    public partial class Form1 : Form
+    {
+        private Image<Bgr, byte> sourceImage; //глобальная переменная
+
+        private VideoCapture capture;
+
+        int frameCount = 0;
+
+        private double cannyThreshold;
+        private double cannyThresholdLinking;
+
+        private universal universal = new universal();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sourceImage = universal.openImg();
+            imageBox1.Image = sourceImage;
+        }
+
+        private void process_Click(object sender, EventArgs e)
+        {
+            imageBox2.Image = universal.effect(sourceImage, cannyThreshold, cannyThresholdLinking); //.Resize(640, 480, Inter.Linear)
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // инициализация веб-камеры
+            capture = new VideoCapture();
+            capture.ImageGrabbed += ProcessFrame;
+            capture.Start(); // начало обработки видеопотока
+        }
+
+        // захват кадра из видеопотока
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            var frame = new Mat();
+            capture.Retrieve(frame); // получение текущего кадра
+
+            Image<Bgr, byte> image = frame.ToImage<Bgr, byte>();
+
+            //Image<Gray, byte> grayImage = image.Convert<Gray, byte>();
+            //var tempImage = grayImage.PyrDown();
+            //var destImage = tempImage.PyrUp();
+            //double cannyThreshold = 80.0;
+            //double cannyThresholdLinking = 40.0;
+            //Image<Gray, byte> cannyEdges = destImage.Canny(cannyThreshold, cannyThresholdLinking);
+            //imageBox2.Image = cannyEdges; //.Resize(640, 480, Inter.Linear)
+            imageBox1.Image = image; //вывод кадра в нужном окне
+            sourceImage = image;
+            imageBox2.Image = universal.effect(sourceImage, cannyThreshold, cannyThresholdLinking);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\Users\\click\\Documents\\2020-2021\\AOCI\\01\\";
+            //openFileDialog.Filter = "mp4 files (*.mp4, *.avi) | *.mpa; *.avi; | All files (*.*)|*.*";
+            var result = openFileDialog.ShowDialog(); // открытие диалога выбора файла
+
+            if (result == DialogResult.OK) // открытие выбранного файла
+            {
+                string fileName = openFileDialog.FileName;
+
+                // инициализация веб-камеры
+                capture = new VideoCapture(fileName);
+                capture.ImageGrabbed += ProcessFrame;
+                capture.Start(); // начало обработки видеопотока
+
+                timer1.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var frame = capture.QueryFrame();
+
+            imageBox2.Image = frame; //.Resize(640, 480, Inter.Linear)
+
+            frameCount++;
+
+            if (frameCount >= capture.GetCaptureProperty(CapProp.FrameCount))
+                timer1.Enabled = false;
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            cannyThreshold = vScrollBar1.Value;
+            textBox1.Text = vScrollBar1.Value.ToString();
+            imageBox2.Image = universal.effect(sourceImage, cannyThreshold, cannyThresholdLinking);
+        }
+
+        private void vScrollBar2_Scroll(object sender, ScrollEventArgs e)
+        {
+            cannyThresholdLinking = vScrollBar2.Value;
+            textBox2.Text = vScrollBar2.Value.ToString();
+            imageBox2.Image = universal.effect(sourceImage, cannyThreshold, cannyThresholdLinking);
+        }
+
+        private void vScrollBar3_Scroll(object sender, ScrollEventArgs e)
+        {
+            universal.a = vScrollBar3.Value;
+            textBox3.Text = vScrollBar3.Value.ToString();
+            imageBox2.Image = universal.effect(sourceImage, cannyThreshold, cannyThresholdLinking);
+        }
+    }
+}
